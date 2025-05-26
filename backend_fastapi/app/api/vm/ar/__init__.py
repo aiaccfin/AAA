6d6a7bede302b69ccc0553081c87bdde64ad2a98
aiaccfin <aiaccfin@gmail.com>
2025-05-26 17:26:00 -0400

    clietn

--- backend_fastapi/app/api/vm/ar/__init__.py
+++ backend_fastapi/app/api/vm/ar/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 
-from app.api.vm.ar import r_invoice
+from app.api.vm.ar import r_invoice, r_aai
 from app.utils.u_auth_py import authent
 
 arRouter = APIRouter()
 
 arRouter.include_router(r_invoice.router, prefix="/invoice",  tags=["xai_ar"], dependencies=[Depends(authent)])
+arRouter.include_router(r_aai.router, prefix="/aai",  tags=["xai_aai"], dependencies=[Depends(authent)])
