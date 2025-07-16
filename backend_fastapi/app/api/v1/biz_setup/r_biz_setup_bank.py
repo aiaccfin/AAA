55b434f6305ad36a3b13d8a0dd8d08db19f86c13
aiaccfin <aiaccfin@gmail.com>
2025-07-16 12:33:07 -0400

    vbank

--- backend_fastapi/app/api/v1/biz_setup/r_biz_setup_bank.py
+++ backend_fastapi/app/api/v1/biz_setup/r_biz_setup_bank.py
@@ -20,6 +20,10 @@ def get_one(id: int, db: Session = Depends(conn.get_session)):
     return crud_biz_bank.get_one(id=id, db=db)
 
 
+# @router.post("/new")
+# def post_new_biz_bank(biz_bank: model_biz_bank.BizBankCreate, db: Session = Depends(conn.get_session)):
+#     return crud_biz_bank.create_one(biz_bank=biz_bank, db=db)
+
 @router.post("/new")
-def post_new_biz_bank(biz_bank: model_biz_bank.BizBankCreate, db: Session = Depends(conn.get_session)):
-    return crud_biz_bank.create_biz_bank(biz_bank=biz_bank, db=db)
+def post_new_biz_bank(Obj: model_biz_bank.BizBankCreate, db: Session = Depends(conn.get_session)):
+    return crud_biz_bank.create_one(Obj=Obj, db=db)
